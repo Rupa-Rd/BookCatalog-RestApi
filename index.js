6c65187ed085a6525fb3755765835f884e9a4594
Rupa-Rd <roopa.69.spr@gmail.com>
2024-05-26 08:41:23 +0530

    Connected to MongoDB Atlas

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const express = require('express')
 const app = express()
 
 mongoose.connect(process.env.DATABASE_URL)
-
 app.listen(3000, ()=> console.log("Server started successfully!"))
 app.use(express.json())
 
